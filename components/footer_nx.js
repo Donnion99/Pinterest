20cfff69df75c993844716ada12b04fbd3710155
Donnion99 <129382800+Donnion99@users.noreply.github.com>
2024-06-06 20:29:35 +0530

    Update footer_nx.js

--- components/footer_nx.js
+++ components/footer_nx.js
@@ -1,32 +1,32 @@
 import React from "react";
 
-function Footer() {
+function Footer_nx() {
   return (
     <footer className="bg-white text-black py-4 mb-12">
-      <div className="container mx-auto px-">
+      <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-center sm:justify-between items-center text-sm font-pacifico font-medium text-center sm:text-left">
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             About
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Careers
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Press
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Help Center
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Privacy & Terms
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Cookies
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             Ads
           </a>
-          <a href="#" className="hover:underline mx-2">
+          <a href="#" className="hover:underline mx-1 sm:mx-2">
             More
           </a>
         </div>
@@ -40,4 +40,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default Footer_nx;
