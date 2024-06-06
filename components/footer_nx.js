b0c72365430df6a18404e1c780b91cfa2a8c39b9
Donnion99 <129382800+Donnion99@users.noreply.github.com>
2024-06-06 20:26:47 +0530

    Update footer_nx.js

--- components/footer_nx.js
+++ components/footer_nx.js
@@ -1,45 +1,43 @@
 import React from "react";
 
-function Footer_nx() {
+function Footer() {
   return (
-    <>
-      <footer class="bg-white text-black py-4 mb-12">
-        <div class="container mx-auto px-4">
-          <div class="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4 text-sm font-pacifico font-medium text-center sm:text-left">
-            <a href="#" class="hover:underline">
-              About
-            </a>
-            <a href="#" class="hover:underline">
-              Careers
-            </a>
-            <a href="#" class="hover:underline">
-              Press
-            </a>
-            <a href="#" class="hover:underline">
-              Help Center
-            </a>
-            <a href="#" class="hover:underline">
-              Privacy & Terms
-            </a>
-            <a href="#" class="hover:underline">
-              Cookies
-            </a>
-            <a href="#" class="hover:underline">
-              Ads
-            </a>
-            <a href="#" class="hover:underline">
-              More
-            </a>
-          </div>
-          <div class="mt-4 text-sm text-gray-500 text-center">
-            <p>
-              &copy; 2024 Pinterest. All rights reserved. Made By Kunal Khude ❤️
-            </p>
-          </div>
+    <footer className="bg-white text-black py-4 mb-12">
+      <div className="container mx-auto px-">
+        <div className="flex flex-wrap justify-center sm:justify-between items-center text-sm font-pacifico font-medium text-center sm:text-left">
+          <a href="#" className="hover:underline mx-2">
+            About
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Careers
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Press
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Help Center
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Privacy & Terms
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Cookies
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            Ads
+          </a>
+          <a href="#" className="hover:underline mx-2">
+            More
+          </a>
         </div>
-      </footer>
-    </>
+        <div className="mt-4 text-sm text-gray-500 text-center">
+          <p>
+            &copy; 2024 Pinterest. All rights reserved. Made By Kunal Khude ❤️
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
-export default Footer_nx;
+export default Footer;
